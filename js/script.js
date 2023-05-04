564571a6a34ae9c11eb89e8027277edbdb9987a3
pratik8376 <131459998+pratik8376@users.noreply.github.com>
2023-05-04 07:56:54 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-/*let sections = document.querySelectorAll('section');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+ 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
@@ -11,13 +20,18 @@ window.onscroll = () => {
         if(top >=offset && top < offset + height){
             navlinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelectorAll('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
-    });*/
-        /* sticky navbar    */
-       /* let header = document.querySelector('header');
+    });
+    
+        let header = document.querySelector('header');
 
         header.classList.toggle('sticky',window.scrollY > 100);
-};*/
+
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
 
